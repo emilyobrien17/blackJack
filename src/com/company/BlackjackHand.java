561a80aca6943acb71991ea9b00c81ddd383a007
emilyobrien17 <e6obrien@student.bridgew.edu>
2018-11-17 18:30:03 -0500

    Riley

---
+++ src/com/company/BlackjackHand.java
@@ -0,0 +1,30 @@
+package com.company;
+
+public class BlackjackHand extends Hand {
+    public BlackjackHand(int maxCards) {
+        super(maxCards);
+    }
+    public int getValue() {
+        int value = 0;
+        for (int i = 0; i < getNumCards(); i++) {
+            var x = getCard(i);
+            if(x.isAce())
+            {
+                if(value > 10) {
+                    value += 1;
+                }
+                else {
+                    value += 11;
+                }
+            }
+            else if(x.isFaceCard()){
+                value += 10;
+            }
+            else {
+                value += x.getDefaultValue();
+            }
+        }
+        return value;
+    }
+
+}
